9c84c66660bb4533ec84ac2d0fad0124499e6c92
Donovan_J-M <110261360+Donojetski13@users.noreply.github.com>
2025-11-17 21:33:52 -0500

    Refactor VS1053 initialization and reset logic

--- HAL_VS1053/Src/VS1053.c
+++ HAL_VS1053/Src/VS1053.c
@@ -36,7 +36,7 @@ uint8_t VS1053_Init()
 	uint16_t status = 0;
 
 	XCS_HIGH;		    /* XCS High */
-//	XDCS_HIGH;		    /* XDCS High */
+//	DREQ_HIGH;		    /* DREQ High */
 	VS1053_Reset();     /* Hard Reset */
 
 	/* x 1.0 Clock, 12MHz / 7, SPI Baudrate should be less than 1.75MHz */
@@ -44,32 +44,33 @@ uint8_t VS1053_Init()
 	if(HAL_SPI_Init(HSPI_VS1053) != HAL_OK) return 13;
 
 	/* Read Status to check SPI */
-	if(!VS1053_SciRead(VS1053_REG_STATUS, &status)) return 13;
+	if(VS1053_SciWrite(VS1053_REG_STATUS, 0x04)) return 13;
+	if(VS1053_SciRead(VS1053_REG_STATUS, &status)) return 13;
 	if(((status >> 4) & 0x0F) != 0x04) return 13;
 
 	/* MP3 Mode GPIO configuration */
-	if(!VS1053_SciWrite(VS1053_REG_WRAMADDR, 0xC017)) return 13; /* GPIO direction */
-	if(!VS1053_SciWrite(VS1053_REG_WRAM, 3)) return 13;
-	if(!VS1053_SciWrite(VS1053_REG_WRAMADDR, 0xC019)) return 13; /* GPIO output */
-	if(!VS1053_SciWrite(VS1053_REG_WRAM, 0)) return 13;
+	if(VS1053_SciWrite(VS1053_REG_WRAMADDR, 0xC017)) return 13; /* GPIO direction */
+	if(VS1053_SciWrite(VS1053_REG_WRAM, 3)) return 13;
+	if(VS1053_SciWrite(VS1053_REG_WRAMADDR, 0xC019)) return 13; /* GPIO output */
+	if(VS1053_SciWrite(VS1053_REG_WRAM, 0)) return 13;
 
 	/* Soft reset */
-	if(!VS1053_SoftReset()) return 13;
+	if(VS1053_SoftReset()) return 13;
 
 	/* x3.0 Clock, 36MHz / 7, SPI Baudrate should be less than 5.14MHz */
-	if(!VS1053_SciWrite(VS1053_REG_CLOCKF, 0x6000)) return 13;
+	if(VS1053_SciWrite(VS1053_REG_CLOCKF, 0x6000)) return 13;
 
 	(HSPI_VS1053)->Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_16;  /* 42MHz / 16 = 2.625MHz */
 	if(HAL_SPI_Init(HSPI_VS1053) != HAL_OK) return 13;
 
 	/* Read Status to check SPI */
-	if(!VS1053_SciRead(VS1053_REG_STATUS, &status)) return 13;
+	if(VS1053_SciRead(VS1053_REG_STATUS, &status)) return 13;
 	if(((status >> 4) & 0x0F) != 0x04) return 13;
 
 	/* Read endFill Byte */
 	uint16_t regVal;
-	if(!VS1053_SciWrite(VS1053_REG_WRAMADDR, 0x1E06)) return 13;	/* endFill */
-	if(!VS1053_SciRead(VS1053_REG_WRAM, &regVal)) return 13;
+	if(VS1053_SciWrite(VS1053_REG_WRAMADDR, 0x1E06)) return 13;	/* endFill */
+	if(VS1053_SciRead(VS1053_REG_WRAM, &regVal)) return 13;
 	endFillByte = regVal & 0xFF;
 
 	return 0;
@@ -79,17 +80,22 @@ uint8_t VS1053_Init()
 void VS1053_Reset()
 {
 	uint8_t dummy = 0xFF;
-	XRST_LOW;		                                    /* XRST Low */
+	XRST_LOW;		/* XRST Low */
+	XCS_LOW;			/* XCS Low */
+
 	HAL_SPI_Transmit(HSPI_VS1053, &dummy, 1, 10);       /* Tx Dummy */
 	HAL_Delay(10);										/* 10ms Delay */
+
 	XRST_HIGH;			                                /* XRST High */
+	XCS_HIGH;			/* XCS High */
+
 	HAL_Delay(10);
 }
 
 /* Soft reset */
 uint8_t VS1053_SoftReset()
 {
-	if(!VS1053_SciWrite(VS1053_REG_MODE, 0x4804)) return 13;	/* SM LINE1 | SM SDINEW | SM RESET */
+	if(VS1053_SciWrite(VS1053_REG_MODE, 0x4804)) return 13;	/* SM LINE1 | SM SDINEW | SM RESET */
 	HAL_Delay(100);
 	return 0;
 }
@@ -100,36 +106,36 @@ uint8_t VS1053_SetVolume(uint8_t volumeLeft, uint8_t volumeRight)
     uint16_t volume;
     volume = ( volumeLeft << 8 ) + volumeRight;
 
-    if(!VS1053_SciWrite(VS1053_REG_VOL, volume)) return 13;
+    if(VS1053_SciWrite(VS1053_REG_VOL, volume)) return 13;
     return 0;
 }
 
 /* Mode control */
 uint8_t VS1053_SetMode(uint16_t mode)
 {
-	if(!VS1053_SciWrite(VS1053_REG_MODE, mode)) return 13;
+	if(VS1053_SciWrite(VS1053_REG_MODE, mode)) return 13;
 	return 0;
 }
 
 uint8_t VS1053_GetMode(uint16_t *mode)
 {
-	if(!VS1053_SciRead(VS1053_REG_MODE, mode)) return 13;
+	if(VS1053_SciRead(VS1053_REG_MODE, mode)) return 13;
 	return 0;
 }
 
 /* Resync control */
 uint8_t VS1053_AutoResync()
 {
-	if(!VS1053_SciWrite(VS1053_REG_WRAMADDR, 0x1E29)) return 13; /* Auto Resync */
-	if(!VS1053_SciWrite(VS1053_REG_WRAM, 0)) return 13;
+	if(VS1053_SciWrite(VS1053_REG_WRAMADDR, 0x1E29)) return 13; /* Auto Resync */
+	if(VS1053_SciWrite(VS1053_REG_WRAM, 0)) return 13;
 	return 0;
 }
 
 /* Set decode time */
 uint8_t VS1053_SetDecodeTime(uint16_t time)
 {
-	if(!VS1053_SciWrite(VS1053_REG_DECODE_TIME, time)) return 13;
-	if(!VS1053_SciWrite(VS1053_REG_DECODE_TIME, time)) return 13;
+	if(VS1053_SciWrite(VS1053_REG_DECODE_TIME, time)) return 13;
+	if(VS1053_SciWrite(VS1053_REG_DECODE_TIME, time)) return 13;
 	return 0;
 }
 
@@ -165,20 +171,20 @@ uint8_t VS1053_SciWrite( uint8_t address, uint16_t input )
 	buffer[2] = input >> 8;			/* Input MSB */
 	buffer[3] = input & 0x00FF;		/* Input LSB */
 
-	while (HAL_GPIO_ReadPin(VS1053_DREQ_PORT, VS1053_DREQ_PIN) == GPIO_PIN_RESET);	/* Wait DREQ High */
+	while(HAL_GPIO_ReadPin(VS1053_DREQ_PORT, VS1053_DREQ_PIN) == GPIO_PIN_RESET);	/* Wait DREQ High */
 
 	XCS_LOW;			/* XCS Low */
 	if(HAL_SPI_Transmit(HSPI_VS1053, buffer, sizeof(buffer), 10) != HAL_OK) return 13;
 	XCS_HIGH;			/* XCS High */
 
-	while (HAL_GPIO_ReadPin(VS1053_DREQ_PORT, VS1053_DREQ_PIN) == GPIO_PIN_RESET);	/* Wait DREQ High */
+	while(HAL_GPIO_ReadPin(VS1053_DREQ_PORT, VS1053_DREQ_PIN) == GPIO_PIN_RESET);	/* Wait DREQ High */
 	return 0;
 }
 
 /* SCI TxRx */
 uint8_t VS1053_SciRead( uint8_t address, uint16_t *res)
 {
-	uint8_t dummy = 0xFF;
+//	uint8_t dummy = 0xFF;
 	uint8_t txBuffer[2];
 	uint8_t rxBuffer[2];
 
@@ -189,8 +195,8 @@ uint8_t VS1053_SciRead( uint8_t address, uint16_t *res)
 
 	XCS_LOW;        /* XCS Low */
 	if(HAL_SPI_Transmit(HSPI_VS1053, txBuffer, sizeof(txBuffer), 10) != HAL_OK) return 13;
-	if(HAL_SPI_TransmitReceive(HSPI_VS1053, &dummy, &rxBuffer[0], 1, 10) != HAL_OK) return 13;
-	if(HAL_SPI_TransmitReceive(HSPI_VS1053, &dummy, &rxBuffer[1], 1, 10) != HAL_OK) return 13;
+	if(HAL_SPI_TransmitReceive(HSPI_VS1053, rxBuffer, &rxBuffer[0], 1, 10) != HAL_OK) return 13;
+	if(HAL_SPI_TransmitReceive(HSPI_VS1053, rxBuffer, &rxBuffer[1], 1, 10) != HAL_OK) return 13;
 	XCS_HIGH;       /* XCS High */
 
 	*res = rxBuffer[0];     /* Received data */
