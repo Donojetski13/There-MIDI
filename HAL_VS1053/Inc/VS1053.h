5f8bb3cfa34408d8ab62abb5347c86bb5e6ad5d3
Donovan_J-M <110261360+Donojetski13@users.noreply.github.com>
2025-11-18 23:38:42 -0500

    Update VS1053 pin definitions and function prototypes

--- HAL_VS1053/Inc/VS1053.h
+++ HAL_VS1053/Inc/VS1053.h
@@ -19,12 +19,32 @@ extern SPI_HandleTypeDef 			hspi2;
 
 #define VS1053_XCS_PORT				GPIOB
 #define VS1053_XCS_PIN				GPIO_PIN_2 // command start
-//#define VS1053_XDCS_PORT			GPIOD
-//#define VS1053_XDCS_PIN			GPIO_PIN_3
+
+#define VS1053_XDCS_PORT			GPIOE
+#define VS1053_XDCS_PIN				GPIO_PIN_2 // Data start
 
 /* Commands */
 #define VS1053_WRITE_CMD	0x02
 #define VS1053_READ_CMD		0x03
+
+/* Registers */
+#define VS1053_REG_BASE		 		((uint8_t) 0x00)
+#define VS1053_REG_MODE   	 		((uint8_t) 0x00) //!< Mode control
+#define VS1053_REG_STATUS 	 		((uint8_t) 0x01)
+#define VS1053_REG_BASS 			((uint8_t) 0x02) //!< Built-in bass/treble control
+#define VS1053_REG_CLOCKF 	 		((uint8_t) 0x03)
+#define VS1053_REG_DECODE_TIME 		((uint8_t) 0x04)
+#define VS1053_REG_AUDATA 	 		((uint8_t) 0x05)
+#define VS1053_REG_WRAM 		 	((uint8_t) 0x06)
+#define VS1053_REG_WRAMADDR 	 	((uint8_t) 0x07)
+#define VS1053_REG_HDAT0 		 	((uint8_t) 0x08)
+#define VS1053_REG_HDAT1 		 	((uint8_t) 0x09)
+#define VS1053_REG_AIADDR 	 		((uint8_t) 0x0A)
+#define VS1053_REG_VOL 		 		((uint8_t) 0x0B) //!< Volume control
+#define VS1053_REG_AICTRL0 	 		((uint8_t) 0x0C)
+#define VS1053_REG_AICTRL1 	 		((uint8_t) 0x0D)
+#define VS1053_REG_AICTRL2 			((uint8_t) 0x0E)
+#define VS1053_REG_AICTRL3 	 		((uint8_t) 0x0F)
 
 /* Functions */
 uint8_t VS1053_Init();
@@ -39,7 +59,7 @@ uint8_t VS1053_SetDecodeTime(uint16_t time);
 uint8_t VS1053_IsBusy();
 uint8_t VS1053_SciWrite(uint8_t address, uint16_t input);
 uint8_t VS1053_SciRead(uint8_t address, uint16_t *res);
-//uint8_t VS1053_SdiWrite(uint8_t input);
+uint8_t VS1053_SdiWrite(uint8_t input);
 //uint8_t VS1053_SdiWrite32(uint8_t *input32);
 
 extern uint8_t endFillByte;
